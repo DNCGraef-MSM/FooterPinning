011c98ffa41118425153e72d2d9f4f21c91dea90
Dan Graef <daniel.graef@moneysupermarket.com>
2019-03-27 15:57:38 +0000

    Initial Commit

---
+++ FooterButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FooterButton
+//
+//  Created by Daniel Graef on 3/27/19.
+//  Copyright © 2019 MoneySuperMarket. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
