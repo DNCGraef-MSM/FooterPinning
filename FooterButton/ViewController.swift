ce9b7be3924358ed75007a6870a938cd3e3f4781
Dan Graef <daniel.graef@moneysupermarket.com>
2019-03-27 16:15:54 +0000

    Getting basic table up

--- FooterButton/ViewController.swift
+++ FooterButton/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    private var footerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        footerView = UIView()
+        footerView.translatesAutoresizingMaskIntoConstraints = false
+        footerView.backgroundColor = .purple
+        footerView.addConstraint(NSLayoutConstraint(item: footerView,
+                                                    attribute: .height,
+                                                    relatedBy: .equal,
+                                                    toItem: nil,
+                                                    attribute: .notAnAttribute,
+                                                    multiplier: 1,
+                                                    constant: 150))
+        view.addSubview(footerView)
+        footerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        footerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        footerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.contentView.backgroundColor = indexPath.row % 2 == 0 ? .red : .blue
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "Cell")!
+    }
 
+    override func viewDidLayoutSubviews() {
+        tableView.isScrollEnabled = tableView.contentSize.height > tableView.bounds.size.height
+        let insets = UIEdgeInsets(top: 0, left: 0, bottom: footerView.bounds.height, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
+    
 }
 
